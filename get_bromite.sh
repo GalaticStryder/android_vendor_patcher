38713e95fbf07862e71a1656aa6c6adfb74ec1d5
Ícaro Hoff <icarohoff@gmail.com>
2018-04-12 16:45:58 -0300

    patcher: update Bromite version to 65.0.3325.218
    Change-Id: Ibe843c2cdf2470858cd71c8c2123eeb267d5be2f
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- get_bromite.sh
+++ get_bromite.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BROMITE_TAG="65.0.3325.204"
+BROMITE_TAG="65.0.3325.218"
 BROMITE_ARCH="arm64"
 BROMITE_FLAVOR="ChromeModernPublic"
 BROMITE_URL="https://github.com/bromite/bromite/releases/download/${BROMITE_TAG}/${BROMITE_ARCH}_${BROMITE_FLAVOR}.apk"
 BROMITE_PATH="prebuilt/app/Bromite.apk"
-BROMITE_SIZE="101197570"
+BROMITE_SIZE="101262082"
 
 function get_bromite {
   if [ "$1" = "--force" ] || [ "$1" = "-F" ]; then

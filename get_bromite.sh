3e54ba20c0742428519c5ba5ab6dae0ed9d610b7
Ícaro Hoff <icarohoff@gmail.com>
2018-07-16 10:43:54 -0300

    patcher: update Bromite browser to 68.0.3440.61
    Change-Id: I2b0de1f925dd4c56bbdb15d4556807bf8f37adb3
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- get_bromite.sh
+++ get_bromite.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BROMITE_TAG="65.0.3325.218"
+BROMITE_TAG="68.0.3440.61"
 BROMITE_ARCH="arm64"
 BROMITE_FLAVOR="ChromeModernPublic"
 BROMITE_URL="https://github.com/bromite/bromite/releases/download/${BROMITE_TAG}/${BROMITE_ARCH}_${BROMITE_FLAVOR}.apk"
 BROMITE_PATH="prebuilt/app/Bromite.apk"
-BROMITE_SIZE="101262082"
+BROMITE_SIZE="100253340"
 
 function get_bromite {
   if [ "$1" = "--force" ] || [ "$1" = "-F" ]; then
